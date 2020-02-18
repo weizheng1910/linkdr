808ce652fca60322744f544986ecfeaa20edb8ef
Stuart Myers <stuart.myers@gmail.com>
2020-02-18 14:47:07 +0800

    Resolve conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :user_candidates
+  devise_for :user_companies
   resources :jobs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
