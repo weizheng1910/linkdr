accafad7069c304992a9f9567a32c0a9cd3642f8
benjacoblee <benjacoblee@gmail.com>
2020-02-18 16:24:16 +0800

    crud for companies. linked companies to user_companies

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   devise_for :user_companies
   resources :jobs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/companies/new" => "companies#new"
+  post "/companies/new" => "companies#create", as:"new_company"
+  get "/companies/:id" => "companies#show", as: "company"
+  get "/companies/:id/edit" => "companies#edit"
+  patch "/companies/:id" => "companies#update"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_18_062817) do
+ActiveRecord::Schema.define(version: 2020_02_18_064300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.string "industry"
+    t.string "size"
+    t.bigint "user_company_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_company_id"], name: "index_companies_on_user_company_id"
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "user_candidates", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_user_candidates_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_user_candidates_on_reset_password_token", unique: true
   end
 
   create_table "user_companies", force: :cascade do |t|
