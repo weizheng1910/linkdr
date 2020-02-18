772fa26b14022721c80454ba6e64a731d0a31e0d
Stuart Myers <stuart.myers@gmail.com>
2020-02-18 14:29:38 +0800

    Added devise user_company as company login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :user_companies
   resources :jobs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
