8c756ee84d7ed25203f0ce861ab4fb19191cf74a
Stuart Myers <stuart.myers@gmail.com>
2020-02-18 16:42:52 +0800

    You can sign up without having a candidate profile

--- app/models/user_candidate.rb
+++ app/models/user_candidate.rb
@@ -1,5 +1,11 @@
 class UserCandidate < ApplicationRecord
-  belongs_to :candidate
+  belongs_to :candidate, optional: true
+  after_create :init_candidate
+
+  def init_candidate
+    self.build_candidate.save(validate: false)
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
