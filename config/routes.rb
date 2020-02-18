14cb61f896f3b4bf3e3899b883a17736173d012b
Stuart Myers <stuart.myers@gmail.com>
2020-02-18 14:59:26 +0800

    Started adding views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :user_candidates
-  devise_for :user_companies
+  devise_for :user_candidates, path: 'user_candidates'
+  devise_for :user_companies, path: 'user_companies'
   resources :jobs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
