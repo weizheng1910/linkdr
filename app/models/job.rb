cb459cf2728be5243511babcabf825f2d09bb2d1
Stuart Myers <stuart.myers@gmail.com>
2020-02-19 21:41:41 +0800

    Works now, promise

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,15 @@
 class Job < ApplicationRecord
   belongs_to :company
   has_many :matches
-  has_and_belongs_to_many :skills, -> { distinct, limit = 5 }
+  has_and_belongs_to_many :skills, -> { distinct }
+
+  after_save :validate_skills
+
+	def validate_skills
+	    if self.skills.length > 5
+	        self.errors.add(:skills, "cannot have more than 5 skills to a job")
+	        return false
+	    end
+	end
 
 end
