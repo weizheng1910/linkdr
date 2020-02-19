80d341bd3b9ec76d9159d1d67fa5b18d3f11b094
benjacoblee <benjacoblee@gmail.com>
2020-02-19 10:51:41 +0800

    made some changes to views

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,14 @@
 class DashboardController < ApplicationController
   def index
+    @home = true
   end
 
   def show
+    if user_company_signed_in?
+      @company = Company.find(params[:id])
+      puts @company.job
+    elsif user_candidate_signed_in?
+      @candidate = Candidate.find(params[:id])
+    end
   end
 end
