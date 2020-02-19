fc5452a093a44db503f04f687da89e55cdf7cfa2
benjacoblee <benjacoblee@gmail.com>
2020-02-19 17:42:40 +0800

    added logic for jobs controller. if current user is candidate, redirect to root

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -14,6 +14,9 @@ class JobsController < ApplicationController
 
   # GET /jobs/new
   def new
+    if current_user_candidate
+      redirect_to "/"
+    end
     @job = Job.new
   end
 
@@ -29,7 +32,7 @@ class JobsController < ApplicationController
 
     respond_to do |format|
       if @job.save
-        format.html { redirect_to @job, notice: 'Job was successfully created.' }
+        format.html { redirect_to @job, notice: "Job was successfully created." }
         format.json { render :show, status: :created, location: @job }
       else
         format.html { render :new }
@@ -43,7 +46,7 @@ class JobsController < ApplicationController
   def update
     respond_to do |format|
       if @job.update(job_params)
-        format.html { redirect_to @job, notice: 'Job was successfully updated.' }
+        format.html { redirect_to @job, notice: "Job was successfully updated." }
         format.json { render :show, status: :ok, location: @job }
       else
         format.html { render :edit }
@@ -57,19 +60,20 @@ class JobsController < ApplicationController
   def destroy
     @job.destroy
     respond_to do |format|
-      format.html { redirect_to jobs_url, notice: 'Job was successfully destroyed.' }
+      format.html { redirect_to jobs_url, notice: "Job was successfully destroyed." }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_job
-      @job = Job.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def job_params
-      params.require(:job).permit(:title, :description)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_job
+    @job = Job.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
 end
