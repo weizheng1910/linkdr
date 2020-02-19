3bf9374fdde9f72ac27b3b8ea4332cc029ae9736
Stuart Myers <stuart.myers@gmail.com>
2020-02-19 13:58:50 +0800

    Loop through and add matching jobs to matches

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -4,6 +4,28 @@ class MatchesController < ApplicationController
   end
 
   def candidatesmatch
+    if current_user_candidate
+      populate_matches_for_candidate ( current_user_candidate )
+    end
     render plain: "candidates match here"
+  end
+
+private
+
+  def populate_matches_for_candidate ( candidate )
+    @jobs = Job.all
+    @jobs.each_with_index do |job, index|
+      match = true
+      job.skills.each do |skill|
+        if candidate.candidate.skills.include? skill
+          next
+        else
+          match = false
+        end
+      end
+      if match == true
+        Match.create(candidate: candidate.candidate, job: job)
+      end
+    end
   end
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -1,5 +1,7 @@
 class Match < ApplicationRecord
   belongs_to :job
   belongs_to :candidate
+  validates :job_id, uniqueness: { scope: :candidate_id }
+
   
 end
