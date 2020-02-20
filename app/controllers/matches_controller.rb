6c472908fb4125666cbecb3c2d568ea7816a8414
Stuart Myers <stuart.myers@gmail.com>
2020-02-20 11:38:04 +0800

    Start changing matches controller

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,4 +1,6 @@
 class MatchesController < ApplicationController
+
+  # Match game for companies
   def companiesmatch
     if current_user_company
       job_id = params[:jobs_id]
@@ -13,6 +15,7 @@ class MatchesController < ApplicationController
     end
   end
 
+  # Match game for candidates
   def candidatesmatch
     if current_user_candidate
       populate_matches_for_candidate ( current_user_candidate )
@@ -25,7 +28,6 @@ class MatchesController < ApplicationController
   end
 
   def likejob
-
     this_match = Match.find(params[:candidate_like][:match_id])
 
     if this_match.candidate_like == false || this_match.candidate_like == nil
@@ -36,8 +38,6 @@ class MatchesController < ApplicationController
 
     id = this_match.candidate_id
     redirect_to '/candidates/' + id.to_s + '/matches'
-
-
   end
 
 private
