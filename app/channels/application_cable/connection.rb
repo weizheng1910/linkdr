abf89bf14c42dc19a20ff98bab2651894cf771c0
weizheng1910 <weizheng1910@gmail.com>
2020-02-21 11:08:16 +0800

    added application_cable/connection.rb

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,21 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+
+    def find_verified_user
+      if verified_user = UserCandidate.find_by(id: cookies.signed['user_candidate.id'])
+        verified_user
+      elsif verified_user = UserCompany.find_by(id: cookies.signed['user_company.id'])
+        verified_user
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
