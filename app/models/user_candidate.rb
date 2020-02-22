760ecdd683247bde4bf9f22701301893eaae1970
benjacoblee <benjacoblee@gmail.com>
2020-02-22 18:14:55 +0800

    removed confirmable from model, change migration schema

--- app/models/user_candidate.rb
+++ app/models/user_candidate.rb
@@ -8,7 +8,7 @@ class UserCandidate < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :confirmable, :registerable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   include Gravtastic
