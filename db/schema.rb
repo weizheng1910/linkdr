7bcaa54afb8e7117323b762e48c3f9330e313eff
Stuart Myers <stuart.myers@gmail.com>
2020-02-22 22:38:58 +0800

    Undo schema.db changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_22_072931) do
+ActiveRecord::Schema.define(version: 2020_02_22_081018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,6 +113,10 @@ ActiveRecord::Schema.define(version: 2020_02_22_072931) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_user_candidates_on_confirmation_token", unique: true
     t.index ["email"], name: "index_user_candidates_on_email", unique: true
     t.index ["reset_password_token"], name: "index_user_candidates_on_reset_password_token", unique: true
   end
